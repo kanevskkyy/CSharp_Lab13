6feded98404ce8d6c7ceb6dd68e6e8632379785c
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-10-12 13:32:17 +0300

    e

---
+++ task 4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using task_4;
+
+class Task
+{
+    static void Main()
+    {
+        List<Person> people = new List<Person>();
+
+        while (true)
+        {
+            string[] enteredInfromation = Console.ReadLine().Split();
+            if (enteredInfromation[0].ToLower() == "end") break;
+            else
+            {
+                Person tempPerson = new Person(enteredInfromation[0], enteredInfromation[1]);
+                people.Add(tempPerson);
+            }
+            Line();
+        }
+
+        Line();
+
+        var result = people
+            .OrderBy(p => p.LastName) 
+            .ThenByDescending(p => p.FirstName);
+
+
+        foreach (var index in result)
+        {
+            Console.WriteLine(index);
+        }
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
---
+++ task 5/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_5
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public Person(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
