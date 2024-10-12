e3eb29b3e9e575f63a9cc84020fa4075c86e76b8
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-10-12 12:45:06 +0300

    done task 1

---
+++ task 1/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int GroupNumber { get; set; }
+
+        public Person(string firstName, string lastName, int groupNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            GroupNumber = groupNumber;
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
--- task 1/Program.cs
+++ task 1/Program.cs
@@ -1,9 +1,35 @@
 ﻿using System;
+using task_1;
 
 class Task
 {
     static void Main()
     {
+        List<Person> people = new List<Person>();
+
+        while(true)
+        {
+            string[] enteredInfromation = Console.ReadLine().Split();
+            if (enteredInfromation[0].ToLower() == "end") break;
+            else
+            {
+                Person tempPerson = new Person(enteredInfromation[0], enteredInfromation[1], int.Parse(enteredInfromation[2]));
+                people.Add(tempPerson);
+            }
+            Line();
+        }
+
+        var result = from i in people
+                     where i.GroupNumber == 2
+                     orderby i.FirstName
+                     select i;
+
+        Console.Write("Result = ");
+        foreach(var index in result)
+        {
+            Console.WriteLine(index);
+        }
+
 
     }
     public static void Line()
